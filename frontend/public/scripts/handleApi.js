8ac5ed85e349bdd8b14f95974a17ae05346ec903
spad1e <pasin.jechou@gmail.com>
2024-04-15 21:13:59 +0700

    feat: render hex board

--- frontend/public/scripts/handleApi.js
+++ frontend/public/scripts/handleApi.js
@@ -1,19 +1,16 @@
 import { BACKEND_URL } from "./config.js";
 
-export async function handleGET() {
-  await fetch(`${BACKEND_URL}/hello`)
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+export async function getPlayer() {
+  await fetch(`${BACKEND_URL}/player`)
+    .then((res) => res.json());
 }
 
-export async function handlePOST() {
-  await fetch(`${BACKEND_URL}/hello`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ sender: "frontend" }),
-  })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+export async function getMap() {
+  await fetch(`${BACKEND_URL}/map`)
+    .then((res) => res.json());
+}
+
+export async function getTile() {
+  await fetch(`${BACKEND_URL}/map`)
+    .then((res) => res.json());
 }
--- frontend/public/scripts/phaser.js
+++ frontend/public/scripts/phaser.js
@@ -1,19 +1,54 @@
-let config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  parent: "gameContainer",
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+class Example extends Phaser.Scene
+{
+    controls;
+
+    preload ()
+    {
+        this.load.image('tiles', '../assets/tileset.png');
+        this.load.tilemapTiledJSON('map', '../assets/hexagonal.json');
+    }
+
+    create ()
+    {
+        const map = this.add.tilemap('map');
+
+        const tileset = map.addTilesetImage('tileset', 'tiles');
+
+        map.createLayer('Calque 1', tileset);
+
+        const cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setZoom(2);
+        this.cameras.main.centerOn(125, 100);
+
+        const controlConfig = {
+            camera: this.cameras.main,
+            left: cursors.left,
+            right: cursors.right,
+            up: cursors.up,
+            down: cursors.down,
+            acceleration: 0.02,
+            drag: 0.0005,
+            maxSpeed: 0.7
+        };
+
+        this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+    }
+
+    update (time, delta)
+    {
+        this.controls.update(delta);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'gameContainer',
+    pixelArt: true,
+    scene: Example
 };
 
-let game = new Phaser.Game(config);
-
-function preload() {}
-
-function create() {}
-
-function update() {}
+const game = new Phaser.Game(config);
