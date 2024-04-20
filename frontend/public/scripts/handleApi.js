c0199ad7d2c4cd38586d8d279e1c97676ecc6216
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-04-20 12:37:37 +0700

    style: unselectable css

--- frontend/public/scripts/handleApi.js
+++ frontend/public/scripts/handleApi.js
@@ -1,9 +1,5 @@
 import { BACKEND_URL } from "./config.js";
 
-// export async function getPlayer() {
-//   await fetch(`${BACKEND_URL}/player`).then((res) => res.json());
-// }
-
 export async function getMap() {
   await fetch(`${BACKEND_URL}/map`).then((res) => res.json());
 }
