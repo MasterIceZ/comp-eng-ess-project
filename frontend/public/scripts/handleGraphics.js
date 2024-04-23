4940842f1d3ad1fc6b340849f40bdb614096f7b0
spad1e <pasin.jechou@gmail.com>
2024-04-23 17:59:17 +0700

    feat: render player turn

--- frontend/public/scripts/handleGraphics.js
+++ frontend/public/scripts/handleGraphics.js
@@ -3,7 +3,7 @@ import { fetchPlayerAPI } from "./handleApi.js";
 import { handleMovePlayer } from "./handleApi.js";
 
 export class handleGraphics {
-  static HEX_RADIUS = 50;
+  static HEX_RADIUS = 45;
   static HEX_BORDER_WIDTH = 4;
   static HEX_BORDER_STROKE_COLOR = 0x20211a;
   static HEX_WIDTH = Math.sqrt(3) * handleGraphics.HEX_RADIUS;
@@ -17,63 +17,21 @@ export class handleGraphics {
   static PLAYER_INFO_COLOR = 0xdcc486;
   static PLAYER_DEAD_INFO_COLOR = 0x808080;
   static ICON_SCALE = 0.02;
-  static ICON_RADIUS = 37.5;
+  static ICON_RADIUS = 35;
   static FONT_FAMILY = "Arial";
+  static TITLE_FONT_FAMILY = "Helvetica";
   static BOARD_COLOR = 0xfff9e3;
 
   static playersOnMap = [];
   static bombs = [];
+  static current_turn = 1;
 
   static async render(scene) {
     await handleGraphics.fetchPlayerData();
-    handleGraphics.renderMap(scene);
     handleGraphics.renderPlayerData(scene);
     handleGraphics.renderPlayerOnBoard(scene);
     handleGraphics.renderBomb(scene);
-  }
-
-  static renderMap(scene) {
-    const points = [];
-    for (let i = 0; i < 6; i++) {
-      const angleRad = (Math.PI / 180) * (60 * i + 30);
-      points.push(handleGraphics.HEX_RADIUS * Math.cos(angleRad));
-      points.push(handleGraphics.HEX_RADIUS * Math.sin(angleRad));
-    }
-
-    for (let i = -3; i <= 3; ++i) {
-      for (let j = -3; j <= 3; ++j) {
-        let color = handleGraphics.BOARD_COLOR;
-        let x =
-          j * handleGraphics.HEX_X_OFFSET +
-          (i % 2 == 0 ? 0 : handleGraphics.HEX_WIDTH / 2);
-        let y = i * handleGraphics.HEX_Y_OFFSET;
-        x += gameUtils.SCREEN_SIZE.w / 2 + handleGraphics.HEX_WIDTH / 2;
-        y += gameUtils.SCREEN_SIZE.h / 2 + handleGraphics.HEX_HEIGHT / 2;
-        const poly = scene.add.polygon(
-          x,
-          y,
-          points,
-          color,
-          1 //opacity
-        );
-        poly.setInteractive(
-          new Phaser.Geom.Polygon(points),
-          Phaser.Geom.Polygon.Contains
-        );
-        poly.setStrokeStyle(
-          handleGraphics.HEX_BORDER_WIDTH,
-          handleGraphics.HEX_BORDER_STROKE_COLOR
-        );
-        poly.on("pointerdown", async () => {
-          await handleMovePlayer(
-            i,
-            j,
-            new URLSearchParams(window.location.search).get("room")
-          );
-          console.log(i, j);
-        });
-      }
-    }
+    handleGraphics.renderPlayerTurn(scene);
   }
 
   static renderPlayerData(scene) {
@@ -156,7 +114,14 @@ export class handleGraphics {
   }
 
   static renderPlayerTurn(scene) {
-    //TODO
+      const text = scene.add
+      .text(gameUtils.SCREEN_SIZE.w/2, gameUtils.SCREEN_SIZE.h*0.95, `${handleGraphics.playersOnMap[handleGraphics.current_turn].name}'s turn to move`, {
+        align: "center",
+        // fontFamily: handleGraphics.TITLE_FONT_FAMILY,
+        fontSize: "32px",
+        fill: "#ff0000",
+      })
+      .setOrigin(0.5, 1);
   }
 
   static renderTitle(scene) {
@@ -186,8 +151,6 @@ export class handleGraphics {
   }
 
   static async fetchPlayerData() {
-    //TODO: link to backend
-    // console.log("fetching map and player data");
     const roomNumber = new URLSearchParams(window.location.search).get("room");
     const data = await fetchPlayerAPI(roomNumber).then((data) => {
       handleGraphics.playersOnMap = data.playersOnMap;
